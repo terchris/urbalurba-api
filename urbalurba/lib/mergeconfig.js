5f44cb39f145e9e03964c7ad03dee050e5e26609
Terje Christensen <terje@christensen.no>
2021-04-21 12:56:32 +0200

    mergeconfig added

---
+++ urbalurba/lib/mergeconfig.js
@@ -0,0 +1,94 @@
+export const MERGECONFIG = {
+    "mergepriority": [
+        "insightly",
+        "strapi",
+        "members_",
+        "brreg",
+        "geonorge",
+        "webpage_"
+    ],
+    "masterRecord": {
+        "domain": "ae.no",
+        "brreg": {
+            "endDate": "2050-04-14",
+            "orgType": "AS,Aksjeselskap",
+            "employees": 167,
+            "foundedDate": "2000-04-14",
+            "organizationNumber": 981952324
+        },
+        "sbn_insightly": "95521295",
+        "idName": "ae.no",
+        "summary": "Agder Energi foredler og forvalter fornybar energi. Konsernets virksomhet omfatter produksjon, distribusjon og salg av fornybar energi, samt energitilknyttede tjenester. Vår visjon er å være et ledend",
+        "socialLinks": {
+            "facebook": "https://www.facebook.com/agderenergi/",
+            "linkedin": "https://www.facebook.com/agderenergi/",
+            "twitter": "https://www.facebook.com/agderenergi/",
+            "instagram": "https://www.facebook.com/agderenergi/",
+            "youtube": "https://www.facebook.com/agderenergi/",
+            "otherURL": "https://www.facebook.com/agderenergi/"
+        },
+        "displayName": "Agder energi",
+        "phone": "38607000",
+        "domains": [
+            "ae.no"
+        ],
+        "urbalurbaIdName": "agder-energi",
+        "web": "http://www.ae.no/",
+        "location": {
+            "latLng": {
+                "lat": "58.1535310346555",
+                "lng": "8.00201645962021"
+            },
+            "adminLocation": {
+                "countyId": "42",
+                "countyName": "Agder",
+                "municipalityId": "4204",
+                "municipalityName": "KRISTIANSAND"
+            },
+            "visitingAddress": {
+                "city": "KRISTIANSAND S",
+                "street": "Kjøita 18",
+                "country": "Norway",
+                "postcode": "4630"
+            }
+        },
+        "image": {
+            "profile": "https://storage.googleapis.com/bucket.urbalurba.com/logo/ae.jpg",
+            "square": "https://storage.googleapis.com/bucket.urbalurba.com/logo/ae.jpg",
+            "cover": "https://storage.googleapis.com/bucket.urbalurba.com/logo/ae.jpg",
+            "icon": "https://storage.googleapis.com/bucket.urbalurba.com/logo/ae.jpg"
+        },
+        "categories": {
+            "sdg": [
+                "11",
+                "17"
+            ],
+            "tag": [
+                "energiselskap"
+            ],
+            "sector": [
+                "private"
+            ],
+            "industry": [
+                "energy"
+            ],
+            "challenge": [
+                "creating-new-jobs",
+                "enabling-new-business-opportunities",
+                "enhances-grid-stability",
+                "improving-energy-supply-efficiency",
+                "improving-energy-usage-efficiency",
+                "promoting-sustainable-disposal-of-waste",
+                "reducing-use-of-fossils",
+                "reducing-use-of-fossils-in-public-transport"
+            ]
+        },
+        "organizationNumber": "981952324",
+        "email": "firmapost@ae.no",
+        "description": "Agder Energi foredler og forvalter fornybar energi. Konsernets virksomhet omfatter produksjon, distribusjon og salg av fornybar energi, samt energitilknyttede tjenester. Vår visjon er å være et ledende norsk konsern innen fornybar energi.\n\n",
+        "slogan": "Fremtidens kraftselskap",
+        "status":"full object",
+        "pages": "full object",
+        "organization": "full object"
+    }
+}
